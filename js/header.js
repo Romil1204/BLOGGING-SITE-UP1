07f39f5c04e85f9287827357137ec6c342f30a38
Romil1204 <romilaggarwalddn@gmail.com>
2019-07-22 00:43:13 +0530

    sign in and sign up done

---
+++ js/header.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("modal");
+var modal1=document.getElementById("modal1");
+// Get the button that opens the modal
+//var btn = document.getElementById("topButtons1");
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+ function show() {
+  modal.style.display = "block";
+}
+ function show1() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+ function hide() {
+  modal.style.display = "none";
+  modal1.style.display = "none";
+}
