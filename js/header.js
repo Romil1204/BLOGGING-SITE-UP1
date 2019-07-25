ae453f8303d2e496d0c4c2a917b7e2a19da71dfa
Romil1204 <romilaggarwalddn@gmail.com>
2019-07-25 21:57:07 +0530

    added functionality of create and all post button

--- js/header.js
+++ js/header.js
@@ -1,21 +1,17 @@
-var modal = document.getElementById("modal");
-var modal1=document.getElementById("modal1");
-// Get the button that opens the modal
-//var btn = document.getElementById("topButtons1");
-
-// Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
- function show() {
-  modal.style.display = "block";
+function openSignIn() {
+    document.getElementById('signInModal').style.display='block';
 }
- function show1() {
-  modal1.style.display = "block";
+    
+function openSignUp() {
+    document.getElementById('signUpModal').style.display='block';
 }
 
-// When the user clicks on <span> (x), close the modal
- function hide() {
-  modal.style.display = "none";
-  modal1.style.display = "none";
+function onCloseModalClicked() {
+    document.getElementById('signInModal').style.display='none';
+    document.getElementById('signUpModal').style.display='none';
 }
+
+function openSignUpButton(){
+    document.getElementById('signUpModal').style.display='block';
+}
+    
